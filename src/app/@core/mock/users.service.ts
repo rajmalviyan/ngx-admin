40435138899d484d301bacb79fbfb5f2db7c6963
RAJ KUMAR <rajmalviyan@gmail.com>
2020-03-04 02:52:56 +0530

    default user updated

--- src/app/@core/mock/users.service.ts
+++ src/app/@core/mock/users.service.ts
@@ -8,8 +8,8 @@ export class UserService extends UserData {
   private time: Date = new Date;
 
   private users = {
-    nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
     raj: { name: 'Raj-D Kumar', picture: 'assets/images/raj.png' },
+    nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
     eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
     jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
     lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
